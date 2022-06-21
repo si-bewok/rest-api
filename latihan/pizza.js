e7f5e7f01c0caa18832acab0c91143f4909404c4
Zhafari <zhafari.16@gmail.com>
2022-06-21 16:13:29 +0700

    Belajar hari ke-2, latihan json

---
+++ latihan/pizza.js
@@ -0,0 +1,55 @@
+function getAllMenu() {
+  $.getJSON("data/pizza.json", function (data) {
+    let menu = data.menu;
+    $.each(menu, function (i, data) {
+      $("#daftar-menu").append(
+        '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' +
+          data.gambar +
+          '" class="card-img-top"><div class="card-body"><h5 class="card-title">' +
+          data.nama +
+          '</h5><p class="card-text">' +
+          data.deskripsi +
+          '</p><h5 class="card-title mb-4">Rp. ' +
+          data.harga +
+          ',-</h5><a href="#" class="btn btn-primary">Pesan sekarang</a></div></div></div>'
+      );
+    });
+  });
+}
+
+getAllMenu();
+
+$(".nav-link").on("click", function () {
+  $(".nav-link").removeClass("active");
+  $(this).addClass("active");
+
+  let kategori = $(this).html();
+  $("h1").html(kategori);
+
+  if (kategori == "All Menu") {
+    getAllMenu();
+    return;
+  }
+
+  $.getJSON("data/pizza.json", function (data) {
+    let menu = data.menu;
+    let content = "";
+
+    $.each(menu, function (i, data) {
+      if (data.kategori == kategori.toLowerCase()) {
+        content +=
+          '<div class="col-md-4"><div class="card mb-3"><img src="img/menu/' +
+          data.gambar +
+          '" class="card-img-top"><div class="card-body"><h5 class="card-title">' +
+          data.nama +
+          '</h5><p class="card-text">' +
+          data.deskripsi +
+          '</p><h5 class="card-title mb-4">Rp. ' +
+          data.harga +
+          ',-</h5><a href="#" class="btn btn-primary">Pesan sekarang</a></div></div></div>';
+      }
+    });
+
+    $("#daftar-menu").html(content);
+  });
+});
